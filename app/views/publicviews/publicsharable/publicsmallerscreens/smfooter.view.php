005c7dfce8caf95a42cc28575259563c4adaef7f
alexokemwa <okemwanalex@gmail.com>
2024-05-22 00:03:48 +0300

    feat: sm screen footer
    this only on small screens accormodating all pages

---
+++ app/views/publicviews/publicsharable/publicsmallerscreens/smfooter.view.php
@@ -0,0 +1,36 @@
+<div class="d-block d-sm-none fixed-bottom">
+  <nav class="navbar navbar-expand-lg bg-success navbar-dark">
+    <div class="container-fluid">
+      <div class="navbar-nav" style="overflow-x: auto;">
+        <ul class="navbar-nav me-auto  mb-lg-0 d-flex flex-row flex-nowrap">
+          <li class="nav-item">
+            <a class="nav-link active px-2" aria-current="page" href="?page_name=home"><i class="bi bi-house-fill"></i>
+              <span class="d-block">Home</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link  px-3"  href="?page_name=orders"><i class="bi bi-calendar-event"></i>
+              <span class="d-block">Events</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link px-3"  href="?page_name=markets"><i class="bi bi-card-text"></i>
+              <span class="d-block">Blog</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link px-3"  href="?page_name=farmerreport"><i class="bi bi-collection"></i>
+              <span class="d-block">resource</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link px-2"  href="?page_name=farmerreport"><i class="bi bi-broadcast"></i>
+              <span class="d-block">socials</span>
+            </a>
+          </li>
+         
+        </ul>
+      </div>
+    </div>
+  </nav>
+</div>
