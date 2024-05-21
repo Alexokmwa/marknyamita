c8055f7fbfc3dda5001b41d4b73314999c6f50ee
alexokemwa <okemwanalex@gmail.com>
2024-05-21 23:55:23 +0300

    feat: separates header and footer for inclusion in different pages

---
+++ app/views/publicviews/publicsharable/publichtmlheaderfooter/footer.view.php
@@ -0,0 +1,6 @@
+
+</body>
+<!-- script files -->
+<script src="<?=ROOT?>/assets/js/bootstrap.bundle.min.js"></script>
+<script src="<?=ROOT?>/assets/js/color-modes.js"></script>
+</html>
---
+++ app/views/publicviews/publicsharable/publichtmlheaderfooter/header.view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?= APP_NAME?></title>
+	<link rel="stylesheet" href="<?=ROOT?>/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?=ROOT?>/assets/css/font/bootstrap-icons.css">
+	<link rel="stylesheet" href="<?=ROOT?>/assets/css/publicmainnav.css">
+	<link rel="stylesheet" href="<?=ROOT?>/assets/css/sign-in.css.css">
+	 
+</head>
+
+<body>
