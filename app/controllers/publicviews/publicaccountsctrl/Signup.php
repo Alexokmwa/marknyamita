46dddbf6435d5544174ecaf04d7c16d6269f674b
alexokemwa <okemwanalex@gmail.com>
2024-05-21 16:03:52 +0300

    feat: add users signup controller
    signup controller to manage signup function to
    mark nyamita followers

---
+++ app/controllers/publicviews/publicaccountsctrl/Signup.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\controllers;
+
+// deny acess to app files and folders access.
+defined('ROOTPATH') or exit('Access Denied!');
+
+use app\core\Controller;
+use app\models\User;
+use app\models\Request;
+
+/**
+ * signup class
+ */
+class Signup extends Controller
+{
+    public function index()
+    {
+        $data['user'] = new User();
+        $req = new Request();
+        if($req->posted()) {
+            $data['user']->signup($_POST);
+        }
+        $this ->view('publicviews/publicaccounts/signup', $data);
+    }
+}
