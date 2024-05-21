8342270f8dc5831362e4fe85b3e4e3ae452903e4
alexokemwa <okemwanalex@gmail.com>
2024-05-21 20:23:48 +0300

    feat: create controller indentifier
    it loops through controllers to launch the right
    requested page from users nd.

---
+++ app/core/App.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace app\core;
+
+// deny acess to app files and folders access.
+defined('ROOTPATH') or exit('Access Denied!');
+/**
+ * app class
+ */
+
+class App
+{
+    private $controller = 'Home';
+    private $method = 'index';
+    private function splitUrl()
+    {
+        // $URL = $_GET['url'] ?? 'Landingpage';
+        $URL = $_GET['url'] ?? 'Home';
+        $URL = explode("/", trim($URL, "/"));
+        return $URL;
+    }
+    public function loadController()
+    {
+        $URL = $this ->splitUrl();
+        //select controller
+        $controllerName =  ucfirst($URL[0]);
+
+        // Array of directories to check for controller files
+        $directories = [
+            "../app/controllers/",
+            "../app/controllers/publicviews/",
+            "../app/controllers/publicviews/publicaccountsctrl/",
+            "../app/controllers/publicviews/publicblogctrl/",
+            "../app/controllers/publicviews/publiceventsctrl/",
+            "../app/controllers/publicviews/publicgeneralpagesctrl/",
+            "../app/controllers/publicviews/publiclibraryctrl/",
+        ];
+
+        $found = false;
+
+        // Iterate over each directory and check if the file exists
+        foreach ($directories as $directory) {
+            $filename = $directory . $controllerName . ".php";
+            if (file_exists($filename)) {
+                require($filename);
+                $this->controller = $controllerName;
+                unset($URL[0]);
+                $found = true;
+                break;
+            }
+        }
+
+        // If not found in any directory, load the NotFound404 controller
+        if (!$found) {
+            require("../app/controllers/NotFound404.php");
+            $this->controller = "NotFound404";
+        }
+        // $filename = "../app/controllers/". $controllerName.".php";
+        // if (file_exists($filename)) {
+        //     require($filename);
+        //     $this -> controller = ucfirst($URL[0]);
+        // } else {
+        //     $filename = "../app/controllers/NotFound404.php";
+        //     require($filename);
+        //     $this ->controller = "NotFound404";
+
+        // }
+
+        $controllerClass = "app\\controllers\\" . $this->controller;
+        $controller = new $controllerClass();
+        //select method
+        if(!empty($URL[1])) {
+            if (method_exists($controller, $URL[1])) {
+                $this->method = $URL[1];
+                unset($URL[1]);
+
+            }
+        }
+        call_user_func_array([$controller, $this -> method], $URL);
+
+    }
+}
