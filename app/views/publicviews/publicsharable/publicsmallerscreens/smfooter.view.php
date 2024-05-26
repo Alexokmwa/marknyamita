235832958e3866913f0e841fe464a0c46a450b57
alexokemwa <okemwanalex@gmail.com>
2024-05-26 12:29:40 +0300

    style: add responsive styles
    for the icons to feat in all possible screens

--- app/views/publicviews/publicsharable/publicsmallerscreens/smfooter.view.php
+++ app/views/publicviews/publicsharable/publicsmallerscreens/smfooter.view.php
@@ -1,34 +1,38 @@
-<div class="d-block d-sm-none fixed-bottom">
+<div class="d-lg-none fixed-bottom">
   <nav class="navbar navbar-expand-lg bg-success navbar-dark">
     <div class="container-fluid">
-      <div class="navbar-nav" style="overflow-x: auto;">
-        <ul class="navbar-nav me-auto  mb-lg-0 d-flex flex-row flex-nowrap">
-          <li class="nav-item">
-            <a class="nav-link active px-2" aria-current="page" href="?page_name=home"><i class="bi bi-house-fill"></i>
+      <div class="navbar-nav d-flex w-100" style="overflow-x: auto;">
+        <ul class="navbar-nav me-auto mb-lg-0 d-flex flex-row flex-nowrap w-100">
+          <li class="nav-item flex-grow-1">
+            <a class="nav-link active d-flex flex-column align-items-center" aria-current="page" href="<?=ROOT?>Home">
+              <i class="bi bi-house-fill"></i>
               <span class="d-block">Home</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link  px-3"  href="?page_name=orders"><i class="bi bi-calendar-event"></i>
+          <li class="nav-item flex-grow-1">
+            <a class="nav-link d-flex flex-column align-items-center" href="<?=ROOT?>Events">
+              <i class="bi bi-calendar-event"></i>
               <span class="d-block">Events</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link px-3"  href="?page_name=markets"><i class="bi bi-card-text"></i>
+          <li class="nav-item flex-grow-1">
+            <a class="nav-link d-flex flex-column align-items-center" href="<?=ROOT?>Blog">
+              <i class="bi bi-card-text"></i>
               <span class="d-block">Blog</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link px-3"  href="?page_name=farmerreport"><i class="bi bi-collection"></i>
-              <span class="d-block">resource</span>
+          <li class="nav-item flex-grow-1">
+            <a class="nav-link d-flex flex-column align-items-center" href="<?=ROOT?>Resource">
+              <i class="bi bi-collection"></i>
+              <span class="d-block">Resource</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link px-2"  href="?page_name=farmerreport"><i class="bi bi-broadcast"></i>
-              <span class="d-block">socials</span>
+          <li class="nav-item flex-grow-1">
+            <a class="nav-link d-flex flex-column align-items-center" href="<?=ROOT?>Socials">
+              <i class="bi bi-broadcast"></i>
+              <span class="d-block">Socials</span>
             </a>
           </li>
-         
         </ul>
       </div>
     </div>
