74d37404f510d1210925d163b0941f43e68a97c5
alexokemwa <okemwanalex@gmail.com>
2024-05-26 13:47:49 +0300

    deafult 404 error display page

--- app/views/404.view.php
+++ app/views/404.view.php
@@ -1,1 +1,15 @@
-view file page not found
+<?php 
+/**
+ * header nav file combined inclusion.
+ */
+renderpageHeader();
+?>
+
+<h1 class="myclassmargintop">view file page not found</h1>
+<?php
+/**
+ * html footer function
+ */
+renderHtmlFooter();
+rendersmfooter();
+?>
