09e48d7ad56b6d1e7e48b039d6ec9ebcda962186
alexokemwa <okemwanalex@gmail.com>
2024-05-30 21:50:08 +0300

    create a view page to be vieed once a page is missng

---
+++ app/views/adminviews/admin404.view.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * header nav file combined inclusion.
+ */
+adminrenderpageHeader();
+?>
+
+<h1 class="myclassmargintop">admin view page not found</h1>
+
+<?php
+/**
+ * html footer function
+ */
+adminrenderHtmlFooter();
+adminrendersmfooter();
+?>
