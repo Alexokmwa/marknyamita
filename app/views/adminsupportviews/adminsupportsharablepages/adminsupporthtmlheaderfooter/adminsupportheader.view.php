e7840c1287c53e218304f1110214a49f9a919cae
alexokemwa <okemwanalex@gmail.com>
2024-05-30 21:38:59 +0300

    create admin support header and footer inclusions

---
+++ app/views/adminsupportviews/adminsupportsharablepages/adminsupporthtmlheaderfooter/adminsupportheader.view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>
+		<?= "Support". " " .APP_NAME?>
+	</title>
+	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
+	<link rel="stylesheet"
+		href="<?=ROOTADMINSUPPORT?>/assets/css/bootstrap.min.css">
+	<link rel="stylesheet"
+		href="<?=ROOTADMINSUPPORT?>/assets/css/font/bootstrap-icons.css">
+	<link rel="stylesheet"
+		href="<?=ROOTADMINSUPPORT?>/assets/css/adminsupportmainnav.css">
+	<link rel="stylesheet"
+		href="<?=ROOTADMINSUPPORT?>/assets/css/adminsupportsign-in.css.css">
+	<link rel="stylesheet"
+		href="<?=ROOTADMINSUPPORT?>/assets/css/adminsupportsmfooter.css">
+
+</head>
+
+<body>
