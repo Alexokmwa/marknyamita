cd1fd351a9a41933c0edaad70626529b375c1c93
alexokemwa <okemwanalex@gmail.com>
2024-05-30 21:41:16 +0300

    create admin support sm screen to md screen view

---
+++ app/views/adminsupportviews/adminsupportsharablepages/adminsupportsmallerscreens/adminsupportsmfooter.view.php
@@ -0,0 +1,40 @@
+<div class="d-lg-none fixed-bottom">
+  <nav class="navbar navbar-expand-lg bg-success navbar-dark">
+    <div class="container-fluid">
+      <div class="navbar-nav d-flex w-100" style="overflow-x: auto;">
+        <ul class="navbar-nav me-auto mb-lg-0 d-flex flex-row flex-nowrap w-100">
+          <li class="nav-item flex-grow-1">
+            <a class="nav-link active d-flex flex-column align-items-center" aria-current="page" href="<?=ROOTADMINSUPPORT?>Adminsupportdashboard">
+            <i class="bi bi-clipboard2-data-fill"></i>
+              <span class="d-block">Dashboard</span>
+            </a>
+          </li>
+          <li class="nav-item flex-grow-1">
+            <a class="nav-link d-flex flex-column align-items-center" href="<?=ROOTADMINSUPPORT?>Adminsupportevents">
+              <i class="bi bi-calendar-event"></i>
+              <span class="d-block">Events</span>
+            </a>
+          </li>
+          <li class="nav-item flex-grow-1">
+            <a class="nav-link d-flex flex-column align-items-center" href="<?=ROOTADMINSUPPORT?>Adminsupportblog">
+              <i class="bi bi-card-text"></i>
+              <span class="d-block">Blog</span>
+            </a>
+          </li>
+          <li class="nav-item flex-grow-1">
+            <a class="nav-link d-flex flex-column align-items-center" href="<?=ROOTADMINSUPPORT?>Adminsupportmedia">
+            <i class="bi bi-play-btn-fill"></i>
+              <span class="d-block">Media</span>
+            </a>
+          </li>
+          <li class="nav-item flex-grow-1">
+            <a class="nav-link d-flex flex-column align-items-center" href="<?=ROOTADMINSUPPORT?>Adminsupportsocials">
+              <i class="bi bi-broadcast"></i>
+              <span class="d-block">Socials</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</div>
