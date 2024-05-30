3abe9274d0bdb7011299f38f9a949569e8732399
alexokemwa <okemwanalex@gmail.com>
2024-05-30 21:29:52 +0300

    feat: create admin support vie pages
    contains admin support view pages

---
+++ app/views/adminsupportviews/adminsupport404.view.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * header nav file combined inclusion.
+ */
+adminsupportrenderpageHeader();
+?>
+
+<h1 class="myclassmargintop">support view page not found</h1>
+
+<?php
+/**
+ * html footer function
+ */
+adminsupportrenderHtmlFooter();
+adminsupportrendersmfooter();
+?>
---
+++ app/views/adminsupportviews/adminsupportgeneralpages/adminsupportblog.view.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * header nav file combined inclusion.
+ */
+adminsupportrenderpageHeader();
+?>
+
+<h1 class="myclassmargintop">support blog management view page design of tabs and subpages goes here</h1>
+
+<?php
+/**
+ * html footer function
+ */
+adminsupportrenderHtmlFooter();
+adminsupportrendersmfooter();
+?>
