ef8d01648504a9514ac8500317c211ff0666ced4
alexokemwa <okemwanalex@gmail.com>
2024-05-30 20:31:49 +0300

    feat: create admin init inclusion file
    includes neccessary core files to be included
    at the entry point of admin

---
+++ app/core/Admininit.php
@@ -0,0 +1,22 @@
+<?php
+
+// deny acess to app files and folders access.
+defined('ROOTPATH') or exit('Access Denied!');
+
+spl_autoload_register(function ($classname) {
+    $filename = '../' . str_replace('\\', '/', $classname) . '.php';
+    if (file_exists($filename)) {
+        require $filename;
+    } else {
+        // Error handling for missing files
+        error_log("Failed to load class: $classname. File not found: $filename");
+    }
+});
+require 'config.php';
+require 'functions.php';
+require 'Database.php';
+require 'Model.php';
+require 'Admincontroller.php';
+require 'Adminapp.php';
+
+
