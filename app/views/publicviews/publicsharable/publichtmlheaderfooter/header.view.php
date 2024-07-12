2a1ad07082b0a923b33969df50ff6cd0c2e89a88
alexokemwa <okemwanalex@gmail.com>
2024-07-12 19:07:38 +0300

    add cdn css plugins

--- app/views/publicviews/publicsharable/publichtmlheaderfooter/header.view.php
+++ app/views/publicviews/publicsharable/publichtmlheaderfooter/header.view.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<!-- $usertitle -->
+
 <head>
 	<title>
+
 		<?= "User" ." " . APP_NAME?>
 	</title>
 	<!-- Meta Tags -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="author" content="primesoftwares.com">
+	<meta name="author" content="primesoftwares.tech">
 	<meta name="description" content="hon mark nyamita campaign platform">
 
 	<!-- Dark mode -->
@@ -70,7 +73,8 @@
 		})
 	</script>
 	<!-- bootstrap cdn  -->
-	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 
 	<!-- Favicon -->
@@ -83,11 +87,11 @@
 		href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;700&amp;family=Rubik:wght@400;500;700&amp;display=swap"
 		rel="stylesheet">
 	<!-- Plugins CSS -->
-	<link rel="stylesheet" type="text/css"
-		href="<?=ROOT?>/assets/asset/vendor/font-awesome/css/all.min.css">
-	<link rel="stylesheet" type="text/css"
-		href="<?=ROOT?>/assets/asset/vendor/bootstrap-icons/bootstrap-icons.css">
-
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+		integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+		crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.css">
 	<!-- Theme CSS -->
 	<link rel="stylesheet" type="text/css"
 		href="<?=ROOT?>/assets/asset/css/style.css">
