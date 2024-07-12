1259eff5c3a0348cc51b4dcd419e684b7984129a
alexokemwa <okemwanalex@gmail.com>
2024-07-12 19:04:54 +0300

    delete margintop function

--- app/views/publicviews/publicaccounts/login.view.php
+++ app/views/publicviews/publicaccounts/login.view.php
@@ -9,81 +9,81 @@ renderpageHeader();
 <h1>Login page view</h1> -->
 <div>
 	<div class="mysmbottomspace">
-		
-		<div class="card shadow container-fluid border col-lg-5 col-md-6 pt-2  myclassmargintop">
-		
+
+		<div class="card shadow container-fluid border col-lg-5 col-md-6 pt-2 ">
+
 			<form method="post">
-		
+
 				<h3 id="mysignupheader"></i>user Login</h3>
 				<br>
 				<div class="mb-3">
-                                 <label for="signinEmailInput" class="form-label">
-                                    Email
-                                    <span class="text-danger">*</span>
-                                 </label>
-                                 
-								 <input value="<?=old_value('email')?>" name="email" type="email" class="form-control" id="signinEmailInput" required />
-        
-                    <div class="text-danger">
-                        <?=$user->getError('email')?>
-                    </div>
-                                 <div class="invalid-feedback">Please enter email.</div>
-                              </div>
-							  <div class="mb-3">
-                                 <label for="formSigninPassword" class="form-label">Password</label>
-                                 <div class="password-field position-relative">
-                                    
-								 <input type="password" class="form-control fakePassword" id="formSigninConfirmPassword" required
-                                    value="<?=old_value('password')?>"
-                                    name="password" />
-                                <div class="text-danger">
-                                    <?=$user->getError('password')?>
-                                </div>
-                                    <div class="invalid-feedback">Please enter password.</div>
-                                 </div>
-                              </div>
-							  <div class="mb-4 d-flex align-items-center justify-content-between">
-                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="rememberMeCheckbox" />
-                                    <label class="form-check-label" for="rememberMeCheckbox">Remember me</label>
-                                 </div>
+					<label for="signinEmailInput" class="form-label">
+						Email
+						<span class="text-danger">*</span>
+					</label>
 
-                                 <div><a href="forget-password.html" class="text-primary">Forgot Password</a></div>
-                              </div>
-							  <div class="d-grid">
-                                 <button class="btn btn-primary" type="submit">Sign In</button>
-                              </div>
+					<input
+						value="<?=old_value('email')?>"
+						name="email" type="email" class="form-control" id="signinEmailInput" required />
+
+					<div class="text-danger">
+						<?=$user->getError('email')?>
+					</div>
+					<div class="invalid-feedback">Please enter email.</div>
+				</div>
+				<div class="mb-3">
+					<label for="formSigninPassword" class="form-label">Password</label>
+					<div class="password-field position-relative">
+
+						<input type="password" class="form-control fakePassword" id="formSigninConfirmPassword" required
+							value="<?=old_value('password')?>"
+							name="password" />
+							<span><i class="bi bi-eye-slash passwordToggler"></i></span>
+
+						<div class="text-danger">
+							<?=$user->getError('password')?>
+						</div>
+						<div class="invalid-feedback">Please enter password.</div>
+					</div>
+				</div>
+				<div class="mb-4 d-flex align-items-center justify-content-between">
+					<div class="form-check">
+						<input
+							<?=old_checked('terms', 1)?>
+						class="form-check-input" type="checkbox" id="rememberMeCheckbox" name="terms"
+						value="1" required/>
+						<div class="text-danger">
+							<?=$user->getError('terms')?>
+						</div>
+						<label class="form-check-label" for="rememberMeCheckbox">Remember me</label>
+
+					</div>
+
+					<div><a href="forget-password.html" class="text-primary">Forgot Password</a></div>
+				</div>
+				<div class="d-grid">
+					<button class="btn btn-primary" type="submit">Sign In</button>
+				</div>
 			</form>
 			<div class="p-3">
 				<div>
-					<p>Create account <a href="<?=ROOT?>Signup">Signup
+					<p>Create account <a
+							href="<?=ROOT?>Signup">Signup
 							Here</a></p>
 				</div>
 			</div>
 		</div>
-			<!-- <form method="post">
-		
-				<input value="<?=old_value('email')?>"
-					name="email" placeHolder="Email"><br>
-				<div><?=$user->getError('email')?></div>
-				<br>
-				<input value="<?=old_value('password')?>"
-					name="password" placeHolder="Password"><br>
-				<div><?=$user->getError('password')?>
-				</div>
-				<br>
-				<button>Login</button>
-			</form> -->
-	</div>
-		<div>
-			<?php
-				/**
-				 * html footer function
-				 */
-				rendermainFooter();
 
-				rendersmfooter();
-				renderHtmlFooter();
-				?>
-		</div>
+	</div>
+	<div>
+		<?php
+                /**
+                 * html footer function
+                 */
+                // rendermainFooter();
+
+rendersmfooter();
+renderHtmlFooter();
+?>
+	</div>
 </div>
