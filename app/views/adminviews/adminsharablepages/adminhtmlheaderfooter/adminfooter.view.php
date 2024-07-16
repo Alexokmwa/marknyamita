976e58ce1f5459a5d2429cf163cda839ada6f966
alexokemwa <okemwanalex@gmail.com>
2024-07-17 01:30:52 +0300

    add jquery cdn

--- app/views/adminviews/adminsharablepages/adminhtmlheaderfooter/adminfooter.view.php
+++ app/views/adminviews/adminsharablepages/adminhtmlheaderfooter/adminfooter.view.php
@@ -12,10 +12,11 @@ JS libraries, plugins and custom scripts -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/3.32.0/apexcharts.min.js"></script>
 <script src="https://cdn.quilljs.com/1.3.6/quill.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/overlayscrollbars/1.13.0/js/OverlayScrollbars.min.js"></script>
-
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <!-- Template Functions -->
 <script src="<?=ROOTADMIN?>/assets/adminassets/js/functions.js"></script>
 <script src="<?=ROOTADMIN?>assets/js/password.js"></script>
+<script src="<?=ROOTADMIN?>assets/js/addcategory.js"></script>
 
 </body>
 
