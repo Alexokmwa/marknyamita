bf391dcc25f7e4b112f49d39176e38174e606808
alexokemwa <okemwanalex@gmail.com>
2024-07-17 01:28:30 +0300

    read data from categories table and insert

--- app/controllers/adminviews/admingeneralpagesctrl/posts/Adminpostcategories.php
+++ app/controllers/adminviews/admingeneralpagesctrl/posts/Adminpostcategories.php
@@ -2,25 +2,34 @@
 
 namespace app\controllers;
 
-// deny acess to app files and folders access.
 defined('ROOTPATH') or exit('Access Denied!');
 
 use app\core\Admincontroller;
 use app\models\Adminsession;
+use app\models\Admincategories;
+use app\models\Request;
 
-/**
- * admin Adminpostcategories management class
- */
 class Adminpostcategories extends Admincontroller
 {
     public function adminindex()
     {
+
         $ses = new Adminsession();
-        if(!$ses->isLoggedIn()) {
+        if (!$ses->isLoggedIn()) {
             redirectadmin('Adminsignin');
         }
+        $user = new Admincategories();
+		$data['row'] = $user->findAllcategories(); 
         $data['admintitle'] = "Admin Categories";
+        $data['admin'] = new Admincategories();
 
-        $this ->adminview('adminviews/admingeneralpages/adminpost/adminpostcategories', $data);
+        $req = new Request();
+        if ($req->posted()) {
+            $result = $data['admin']->adminaddcategory($req->POST());
+            echo json_encode($result);
+            exit;
+        }
+
+        $this->adminview('adminviews/admingeneralpages/adminpost/adminpostcategories', $data);
     }
 }
