889db93cb452c25224c3b2fc7cdf23d69dbcbfd8
alexokemwa <okemwanalex@gmail.com>
2024-07-24 14:37:30 +0300

    add cdn links neccesssary

--- app/views/adminviews/adminsharablepages/adminhtmlheaderfooter/adminfooter.view.php
+++ app/views/adminviews/adminsharablepages/adminhtmlheaderfooter/adminfooter.view.php
@@ -11,9 +11,13 @@ JS libraries, plugins and custom scripts -->
 <!-- Vendors -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/3.32.0/apexcharts.min.js"></script>
 <script src="https://cdn.quilljs.com/1.3.6/quill.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/overlayscrollbars/1.13.0/js/OverlayScrollbars.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <!-- Template Functions -->
+ <!-- vanilla lazy load  -->
+ <script src="https://cdn.jsdelivr.net/npm/vanilla-lazyload@17.5.2/dist/lazyload.min.js"></script>
+
 <script src="<?=ROOTADMIN?>/assets/adminassets/js/functions.js"></script>
 <script src="<?=ROOTADMIN?>assets/js/password.js"></script>
 <script src="<?=ROOTADMIN?>assets/js/addcategory.js"></script>
