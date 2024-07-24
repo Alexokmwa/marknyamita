dade57c7c7d570eaea2ad4112d93fed733e98f3c
alexokemwa <okemwanalex@gmail.com>
2024-07-24 14:26:58 +0300

    create posts

--- app/controllers/adminviews/admingeneralpagesctrl/posts/Admincreatepost.php
+++ app/controllers/adminviews/admingeneralpagesctrl/posts/Admincreatepost.php
@@ -2,25 +2,45 @@
 
 namespace app\controllers;
 
-// deny acess to app files and folders access.
 defined('ROOTPATH') or exit('Access Denied!');
-
+use app\models\Admincategories;
 use app\core\Admincontroller;
+use app\models\Request;
 use app\models\Adminsession;
+use app\models\Adminpostsmodel;
 
 /**
- * admin Admincreatepost management class
+ *  Admincreatepost management class
  */
 class Admincreatepost extends Admincontroller
 {
     public function adminindex()
     {
         $ses = new Adminsession();
-        if(!$ses->isLoggedIn()) {
+        if (!$ses->isLoggedIn()) {
             redirectadmin('Adminsignin');
         }
+        $user = new Admincategories();
+        $data['row'] = $user->findAllcategories();
+        $adminModel = new Adminpostsmodel();
+
+        $req = new Request();
+        if ($req->posted()) {
+            $adminID = $ses->adminuser("adminID");
+            $files = $req->files();
+            $postData = $req->POST();
+            $postDataquil = $req->POST("postbody");
+
+            // Ensure adminID is set in POST data
+            $postData["adminID"] = $adminID;
+            $postData["postbody"] = $postDataquil; // Store plain text content
+            $adminModel->adminaddpost($postData, $files, $adminID);
+        }
+
+        $data['admin'] = $adminModel;
         $data['admintitle'] = "Admin Createpost";
 
-        $this ->adminview('adminviews/admingeneralpages/adminpost/admincreatepost', $data);
+        // Render view with data
+        $this->adminview('adminviews/admingeneralpages/adminpost/admincreatepost', $data);
     }
 }
