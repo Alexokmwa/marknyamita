476fec379d1ef24c2e32ca31362205e4b88d7894
alexokemwa <okemwanalex@gmail.com>
2024-07-24 14:36:00 +0300

    blog dynamic loading of data

--- app/views/adminviews/admingeneralpages/adminpages/adminblog.view.php
+++ app/views/adminviews/admingeneralpages/adminpages/adminblog.view.php
@@ -5,7 +5,71 @@
 adminrenderpageHeader();
 ?>
 
-<h1 class="myclassmargintop">admin blog view page design of tabs and subpages goes here</h1>
+
+<main class="mysmbottomspace">
+	<section class="pt-4">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<a href="<?=ROOTADMIN?>Admindashboard"
+						class="d-block text-decoration-none">
+						<div class="mybgblog bg-opacity-10 p-4 text-center rounded-3 border border-danger">
+							<h1 class="text-danger m-0">DASHBOARD</h1>
+							<p class="lead m-0">Checkout site analytics</p>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="pt-1">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<a href="<?=ROOTADMIN?>Adminpostcategories"
+						class="d-block text-decoration-none">
+						<div class="mybgblog bg-opacity-10 p-4 text-center rounded-3 border border-danger">
+							<h1 class="text-danger m-0">BLOG CATEGORIES</h1>
+							<p class="lead m-0">view and create categories</p>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="pt-1">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<a href="<?=ROOTADMIN?>Admincreatepost"
+						class="d-block text-decoration-none">
+						<div class="mybgblog bg-opacity-10 p-4 text-center rounded-3 border border-danger">
+							<h1 class="text-danger m-0">BLOG CREATE POST</h1>
+							<p class="lead m-0">Create post</p>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="pt-1">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<a href="<?=ROOTADMIN?>Adminpostlist"
+						class="d-block text-decoration-none">
+						<div class="mybgblog bg-opacity-10 p-4 text-center rounded-3 border border-danger">
+							<h1 class="text-danger m-0">BLOG POST LIST</h1>
+							<p class="lead m-0">Checkout blog list and annalytics</p>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+</main>
 
 <?php
 /**
