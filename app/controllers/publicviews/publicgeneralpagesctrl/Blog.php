240738dbf1b0125e9fb5eb7f93e21c7d6873bb2b
alexokemwa <okemwanalex@gmail.com>
2024-07-24 14:28:32 +0300

    retrive and show blog

--- app/controllers/publicviews/publicgeneralpagesctrl/Blog.php
+++ app/controllers/publicviews/publicgeneralpagesctrl/Blog.php
@@ -6,6 +6,9 @@ namespace app\controllers;
 defined('ROOTPATH') or exit('Access Denied!');
 
 use app\core\Controller;
+use app\models\Adminpostsmodel;
+use app\models\Admincategories;
+use app\models\Adminaccounts;
 
 /**
  * Blog class
@@ -14,7 +17,13 @@ class Blog extends Controller
 {
     public function index()
     {
-
-        $this ->view('publicviews/publicgeneralpages/blog');
+        $user = new Admincategories();
+        $data['row'] = $user->findAllcategories();
+        $userpost = new Adminpostsmodel();
+        $data['rowpost'] = $userpost->findAllposts();
+        $adminpostdetail = new Adminaccounts();
+        $data['rowcreator'] = $adminpostdetail->findAlladmin();
+        show($data);
+        $this ->view('publicviews/publicgeneralpages/blog', $data);
     }
 }
