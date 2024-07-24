e8c54fa0378eceef4ecbdd9b348f57ccbf59a225
alexokemwa <okemwanalex@gmail.com>
2024-07-24 14:39:25 +0300

    update main footer content

--- app/views/publicviews/publicsharable/publicnavfooters/mainfooter.view.php
+++ app/views/publicviews/publicsharable/publicnavfooters/mainfooter.view.php
@@ -7,10 +7,12 @@
             <div class="mb-7 mb-xl-0">
                 <div class="mb-4">
                     <a href="#">
+                    <!-- <img src="<?=ROOT?>assets/logo/marknyamitalogo.jpeg" alt="logo" class="text-inverse" /> -->
+
                         <img src="../assets/images/logo/logo.svg" alt="logo" class="text-inverse" />
                     </a>
                 </div>
-                <p class="mb-5">Build faster websites with Block multipurpose bootstrap 5 template. Duis imper diet mollis leo, quis ultrices erat ultrices simple dummy .</p>
+                <p class="mb-5"> The platform hosts campaign videos, photos, live event links, blog posts, and more, Stay connected and informed with easy access to all campaign materials, news, and live events, all in one place.</p>
 
                 <form class="needs-validation" novalidate>
                     <h5 class="mb-3">Subscribe to our newsletter</h5>
@@ -34,7 +36,7 @@
                 <div class="col-lg-4 col-12">
                     <div class="position-relative">
                         <div class="mb-3 pb-2 d-flex justify-content-between border-bottom border-bottom-lg-0">
-                            <h5>Landings</h5>
+                            <h5>Home</h5>
                             <a
                                 class="d-block d-lg-none stretched-link text-inherit"
                                 data-bs-toggle="collapse"
@@ -48,29 +50,111 @@
                         <div class="collapse d-lg-block" id="collapseLanding" data-bs-parent="#ft-links">
                             <ul class="list-unstyled mb-0 py-3 py-lg-0">
                                 <li class="mb-2">
-                                    <a href="../index.html" class="text-decoration-none text-reset">Saas</a>
+                                    <a href="<?=ROOT?>Home" class="text-decoration-none text-reset">home</a>
                                 </li>
+                                
+                            </ul>
+                        </div>
+                        <div class="mb-3 pb-2 d-flex justify-content-between border-bottom border-bottom-lg-0 pt-4">
+                            <h5>ABOUT</h5>
+                            <a
+                                class="d-block d-lg-none stretched-link text-inherit"
+                                data-bs-toggle="collapse"
+                                href="#collapseLanding"
+                                role="button"
+                                aria-expanded="false"
+                                aria-controls="collapseLanding">
+                                <i class="bi bi-chevron-down"></i>
+                            </a>
+                        </div>
+                        <div class="collapse d-lg-block" id="collapseLanding" data-bs-parent="#ft-links">
+                            <ul class="list-unstyled mb-0 py-3 py-lg-0">
                                 <li class="mb-2">
-                                    <a href="#!" class="text-decoration-none text-reset">Software Company</a>
+                                    <a href="<?=ROOT?>Aboutus" class="text-decoration-none text-reset">home</a>
                                 </li>
-                                <li class="mb-2">
-                                    <a href="../landing-finance.html" class="text-decoration-none text-reset">Finance</a>
-                                </li>
-                                <li class="mb-2">
-                                    <a href="../landing-jamstack-agancy.html" class="text-decoration-none text-reset">Digital Agency</a>
-                                </li>
-                                <li class="mb-2">
-                                    <a href="../landing-conference.html" class="text-decoration-none text-reset">Conference</a>
-                                </li>
-                                <li class="mb-2">
-                                    <a href="#!" class="text-decoration-none text-reset">App Template</a>
-                                </li>
+                                
                             </ul>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-12">
                     <div class="mb-3 pb-2 d-flex justify-content-between border-bottom border-bottom-lg-0 position-relative">
+                        <h5>Events</h5>
+                        <a
+                            class="d-block d-lg-none stretched-link text-inherit"
+                            data-bs-toggle="collapse"
+                            href="#collapseAccounts"
+                            role="button"
+                            aria-expanded="false"
+                            aria-controls="collapseAccounts">
+                            <i class="bi bi-chevron-down"></i>
+                        </a>
+                    </div>
+                    <div class="collapse d-lg-block" id="collapseAccounts" data-bs-parent="#ft-links">
+                        <ul class="list-unstyled mb-0 py-3 py-lg-0">
+                            <li class="mb-2">
+                                <a href="<?=ROOT?>Events" class="text-decoration-none text-reset">events</a>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                    <div class="mb-3 pb-2 d-flex justify-content-between border-bottom border-bottom-lg-0 position-relative pt-4">
+                        <h5>Library</h5>
+                        <a
+                            class="d-block d-lg-none stretched-link text-inherit"
+                            data-bs-toggle="collapse"
+                            href="#collapseResources"
+                            role="button"
+                            aria-expanded="false"
+                            aria-controls="collapseResources">
+                            <i class="bi bi-chevron-down"></i>
+                        </a>
+                    </div>
+                    <div class="collapse d-lg-block" id="collapseResources" data-bs-parent="#ft-links">
+                        <ul class="list-unstyled mb-0 py-3 py-lg-0">
+                            <li class="mb-2">
+                                <a href="<?=ROOT?>Resource" class="text-decoration-none text-reset">Library</a>
+                            </li>
+                            <!-- <li class="mb-2">
+                                <a href="#!" class="text-decoration-none text-reset">Support</a>
+                            </li>
+                            <li class="mb-2">
+                                <a href="../changelog.html" class="text-decoration-none text-reset">Changelog</a>
+                            </li>
+                            <li class="mb-2">
+                                <a href="#!" class="text-decoration-none text-reset">Help Center</a>
+                            </li>
+                            <li class="mb-2">
+                                <a href="#!" class="text-decoration-none text-reset">Community</a>
+                            </li>
+                            <li class="mb-2">
+                                <a href="#!" class="text-decoration-none text-reset">Webinars</a>
+                            </li> -->
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-12">
+                    <div class="mb-3 pb-2 d-flex justify-content-between border-bottom border-bottom-lg-0 position-relative">
+                        <h5>Blog</h5>
+                        <a
+                            class="d-block d-lg-none stretched-link text-inherit"
+                            data-bs-toggle="collapse"
+                            href="#collapseResources"
+                            role="button"
+                            aria-expanded="false"
+                            aria-controls="collapseResources">
+                            <i class="bi bi-chevron-down"></i>
+                        </a>
+                    </div>
+                    <div class="collapse d-lg-block" id="collapseResources" data-bs-parent="#ft-links">
+                        <ul class="list-unstyled mb-0 py-3 py-lg-0">
+                            <li class="mb-2">
+                                <a href="../docs/index.html" class="text-decoration-none text-reset">blog</a>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                    <div class="mb-3 pb-2 d-flex justify-content-between border-bottom border-bottom-lg-0 position-relative pt-4">
                         <h5>Accounts</h5>
                         <a
                             class="d-block d-lg-none stretched-link text-inherit"
@@ -85,55 +169,19 @@
                     <div class="collapse d-lg-block" id="collapseAccounts" data-bs-parent="#ft-links">
                         <ul class="list-unstyled mb-0 py-3 py-lg-0">
                             <li class="mb-2">
-                                <a href="../signup.html" class="text-decoration-none text-reset">Register</a>
+                                <a href="<?=ROOT?>Signup" class="text-decoration-none text-reset">Register</a>
                             </li>
                             <li class="mb-2">
-                                <a href="../signin.html" class="text-decoration-none text-reset">Login</a>
+                                <a href="<?=ROOT?>Login" class="text-decoration-none text-reset">Login</a>
                             </li>
                             <li class="mb-2">
-                                <a href="../forget-password.html" class="text-decoration-none text-reset">Forgot Password</a>
+                                <a href="#!" class="text-decoration-none text-reset">Forgot Password</a>
                             </li>
                             <li class="mb-2">
-                                <a href="../reset-password.html" class="text-decoration-none text-reset">Reset Password</a>
+                                <a href="#!" class="text-decoration-none text-reset">Reset Password</a>
                             </li>
                             <li class="mb-2">
                                 <a href="#!" class="text-decoration-none text-reset">Profile</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-12">
-                    <div class="mb-3 pb-2 d-flex justify-content-between border-bottom border-bottom-lg-0 position-relative">
-                        <h5>Resources</h5>
-                        <a
-                            class="d-block d-lg-none stretched-link text-inherit"
-                            data-bs-toggle="collapse"
-                            href="#collapseResources"
-                            role="button"
-                            aria-expanded="false"
-                            aria-controls="collapseResources">
-                            <i class="bi bi-chevron-down"></i>
-                        </a>
-                    </div>
-                    <div class="collapse d-lg-block" id="collapseResources" data-bs-parent="#ft-links">
-                        <ul class="list-unstyled mb-0 py-3 py-lg-0">
-                            <li class="mb-2">
-                                <a href="../docs/index.html" class="text-decoration-none text-reset">Docs</a>
-                            </li>
-                            <li class="mb-2">
-                                <a href="#!" class="text-decoration-none text-reset">Support</a>
-                            </li>
-                            <li class="mb-2">
-                                <a href="../changelog.html" class="text-decoration-none text-reset">Changelog</a>
-                            </li>
-                            <li class="mb-2">
-                                <a href="#!" class="text-decoration-none text-reset">Help Center</a>
-                            </li>
-                            <li class="mb-2">
-                                <a href="#!" class="text-decoration-none text-reset">Community</a>
-                            </li>
-                            <li class="mb-2">
-                                <a href="#!" class="text-decoration-none text-reset">Webinars</a>
                             </li>
                         </ul>
                     </div>
@@ -146,11 +194,12 @@
     <div class="row align-items-center">
         <div class="col-md-9">
             <div class="small mb-3 mb-md-0">
-                Copyright © 2023
+                Copyright © 2024
 
-                <span class="text-primary"><a href="#">Block Bootstrap 5 Theme</a></span>
+                <span class="text-primary"><a href="#">primesoftwares</a></span>
                 | Designed by
-                <span class="text-primary"><a href="#">CodesCandy</a></span>
+                <span class="text-primary"><a href="https://www.primesoftwares.tech">Primesoftwares</a></span>
+
             </div>
         </div>
         <div class="col-md-3">
@@ -193,7 +242,7 @@
                                 d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
                         </svg>
                     </a>
-                    <a href="#!" class="text-reset btn btn-facebook btn-icon">
+                    <a href="https://www.facebook.com/NyamitaMark?mibextid=rS40aB7S9Ucbxw6v" class="text-reset btn btn-facebook btn-icon" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
                             <path
                                 d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
