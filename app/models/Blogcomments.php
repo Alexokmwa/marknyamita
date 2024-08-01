6c600cc86d3361222e9e80a8e86d29ee4b260db9
alexokemwa <okemwanalex@gmail.com>
2024-08-01 23:28:57 +0300

    Blogcomments models
    to write data to the database

---
+++ app/models/Blogcomments.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace app\models;
+
+defined('ROOTPATH') or exit('Access Denied!');
+use app\core\Model;
+use app\models\Image;
+
+class Blogcomments
+{
+    use Model;
+
+    protected $table = 'comments';
+    protected $primaryKey = 'commentID';
+    protected $loginUniqueColumn = 'commentID';
+
+    protected $allowedColumns = [
+        'name',
+        'email',
+        'postID',
+        'user_id',
+        'Content',
+        'usercommentimage',
+
+    ];
+
+    protected $validationRules = [
+        'name' => [
+            'alpha',
+            'required',
+        ],
+        'email' => [
+           'email',
+            'required',
+        ],
+        'Content' => [
+            'required',
+        ],
+        'usercommentimage' => [
+            'required',
+        ],
+
+
+    ];
+
+    public function addblogcomment($data, $files, $userID, $postid)
+    {
+        if ($this->validate($data)) {
+            $data['date'] = date("Y-m-d H:i:s");
+            $data['date_created'] = date("Y-m-d H:i:s");
+            $data["user_id"] = $userID;
+            $data["postID"] = $postid;
+
+            if (!empty($files["usercommentimage"]["name"])) {
+                $folder = "public/commentuploads/";
+                if (!file_exists($folder)) {
+                    mkdir($folder, 0777, true);
+                    file_put_contents($folder . 'index.php', "");
+                }
+                $allowed = ['image/jpeg', 'image/png', 'image/gif', 'image/webp'];
+                if (in_array($files["usercommentimage"]["type"], $allowed)) {
+                    $destination = $folder . time() . $files["usercommentimage"]["name"];
+                    if (move_uploaded_file($files["usercommentimage"]['tmp_name'], $destination)) {
+                        $data["usercommentimage"] = $destination;
+                        $image = new Image();
+                        $image->resize($data["usercommentimage"], 700);
+                    }
+                    $this->insert($data);
+                    redirect('Blogview/'.$postid);
+
+                } else {
+                    $this->errors["usercommentimage"] = "the file type is not supported";
+                }
+            }
+        } else {
+            $this->addError('usercommentimage', "no image found, upload at least one image");
+            $this->addError('name', "no image found, upload at least one image");
+
+        }
+    }
+}
