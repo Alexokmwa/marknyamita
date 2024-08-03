5fcfb93e475220fdf635cebccd2df7451fe0fc20
alexokemwa <okemwanalex@gmail.com>
2024-08-04 01:24:53 +0300

    update data to creect value to the function postdata

--- app/models/Blogloggedinreplies.php
+++ app/models/Blogloggedinreplies.php
@@ -37,14 +37,14 @@ class Blogloggedinreplies
         ],
     ];
 
-    public function addblogcommentreply($data,$commentid, $files, $userID, $postid)
+    public function addblogcommentreply($postData,$commentid, $files, $userID, $postid)
     {
-        if ($this->validate($data)) {
-            $data['date'] = date("Y-m-d H:i:s");
-            $data['date_created'] = date("Y-m-d H:i:s");
-            $data["user_id"] = $userID;
-            $data["commentID"] = $commentid;
-            $data["usercommentimage"] = "";
+        if ($this->validate($postData)) {
+            $postData['date'] = date("Y-m-d H:i:s");
+            $postData['date_created'] = date("Y-m-d H:i:s");
+            $postData["user_id"] = $userID;
+            $postData["commentID"] = $commentid;
+            $postData["usercommentimage"] = "";
 
             if (!empty($files["usercommentimage"]["name"])) {
                 $folder = "public/commentuploads/replieslogged/";
@@ -56,9 +56,9 @@ class Blogloggedinreplies
                 if (in_array($files["usercommentimage"]["type"], $allowed)) {
                     $destination = $folder . time() . $files["usercommentimage"]["name"];
                     if (move_uploaded_file($files["usercommentimage"]['tmp_name'], $destination)) {
-                        $data["usercommentimage"] = $destination;
+                        $postData["usercommentimage"] = $destination;
                         $image = new Image();
-                        $image->resize($data["usercommentimage"], 700);
+                        $image->resize($postData["usercommentimage"], 700);
                     }
                 } else {
                     $this->errors["usercommentimage"] = "The file type is not supported.";
@@ -66,12 +66,86 @@ class Blogloggedinreplies
             }
 
             if (empty($this->errors)) {
-                $this->insert($data);
+                $this->insert($postData);
                 $ses = new Session();
                 $ses->set('comment_success', 'Your reply comment has been posted successfully.');
                 
                 redirect('Blogview/'.$postid);
             }
         }
+    }
+    public function editblogcommentreply($idupdatereply,$postData, $commentid, $files, $userID, $postid)
+    {
+        if ($this->validate($postData)) {
+            $postData['date'] = date("Y-m-d H:i:s");
+            $postData['date_created'] = date("Y-m-d H:i:s");
+            $postData["user_id"] = $userID;
+            $postData["postID"] = $postid;
+            // $postData["usercommentimage"] = "";
+
+           // Fetch the current image URL from the database
+           $currentPost = $this->first([$this->primaryKey =>  $idupdatereply]);
+           if ($currentPost) {
+               $currentusercommentimage = $currentPost->usercommentimage;
+           }
+           if (!empty($files["usercommentimage"]["name"])) {
+            $folder = "public/commentuploads/replieslogged/";
+
+ if (!file_exists($folder)) {
+                   mkdir($folder, 0777, true);
+                   file_put_contents($folder . 'index.php', "");
+               }
+               $allowed = ['image/jpeg', 'image/png', 'image/gif', 'image/webp'];
+               if (in_array($files["usercommentimage"]["type"], $allowed)) {
+                   $destination = $folder . time() . $files["usercommentimage"]["name"];
+                   if (move_uploaded_file($files["usercommentimage"]['tmp_name'], $destination)) {
+                       $postData["usercommentimage"] = $destination;
+                       $image = new Image();
+                       $image->resize($postData["usercommentimage"], 700);
+
+                       // Delete the old image if it exists
+                       if (file_exists($currentusercommentimage)) {
+                           unlink($currentusercommentimage);
+                       }
+                   }
+                } else {
+                    $this->errors["usercommentimage"] = "the file type is not supported";
+                }
+            }
+            
+            if (empty($this->errors)) {
+                $this->updatecommentreply( $idupdatereply, $postData);
+                $ses = new Session();
+                $ses->set('comment_success', 'Your comment has been edited successfully.');
+                
+                redirect('Blogview/'.$postid);
+            }
+        }
+    }
+    public function deletereply($commentID,$postid)
+    {
+    
+            // Fetch the current image URL from the database
+            $currentPost = $this->first([$this->primaryKey => $commentID]);
+    
+            if ($currentPost) {
+                $currentusercommentimage = $currentPost->usercommentimage;
+    
+                // Delete the old image if it exists
+                if ($currentusercommentimage && file_exists($currentusercommentimage)) {
+                    unlink($currentusercommentimage);
+                }
+    
+                // Delete the post from the database
+                
+                if (empty($this->errors)) {
+                    $this->deletecommentreply($commentID);
+                    $ses = new Session();
+                    $ses->set('comment_success', 'Your reply comment has been posted successfully.');
+                    
+                    redirect('Blogview/'.$postid);
+                }
+            } 
+        
     }
 }
