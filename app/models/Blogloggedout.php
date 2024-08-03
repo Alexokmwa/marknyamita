ee0b192845c5ffe932ae2316e4eeb86836d23ea3
alexokemwa <okemwanalex@gmail.com>
2024-08-03 21:38:58 +0300

    add blog replies modals

---
+++ app/models/Blogloggedout.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace app\models;
+
+defined('ROOTPATH') or exit('Access Denied!');
+use app\core\Model;
+use app\models\Image;
+use app\models\Session;
+
+class Blogloggedout
+{
+    use Model;
+
+    protected $table = 'commentrepliesnotloggedin';
+    protected $primaryKey = 'replyID';
+    protected $loginUniqueColumn = 'replyID';
+
+
+    protected $allowedColumns = [
+        'namereply',
+        'emailreply',
+        'commentID',
+        'user_id',
+        'Contentreply',
+        'usercommentimage',
+    ];
+    protected $validationRules = [
+        'namereply' => [
+            'alpha_space',
+            'required',
+        ],
+        'emailreply' => [
+           'email',
+            'required',
+        ],
+        'Contentreply' => [
+            'required',
+        ],
+    ];
+
+
+    public function addblogcommentnotloggedinreply($data,$commentid, $files, $userID, $postid)
+    {
+        if ($this->validate($data)) {
+            $data['date'] = date("Y-m-d H:i:s");
+            $data['date_created'] = date("Y-m-d H:i:s");
+            $data["user_id"] = $userID;
+            $data["commentID"] = $commentid;
+
+            $data["usercommentimage"] = "";
+
+            if (!empty($files["usercommentimage"]["name"])) {
+                $folder = "public/commentuploads/repliesnotlogged/";
+                if (!file_exists($folder)) {
+                    mkdir($folder, 0777, true);
+                    file_put_contents($folder . 'index.php', "");
+                }
+                $allowed = ['image/jpeg', 'image/png', 'image/gif', 'image/webp'];
+                if (in_array($files["usercommentimage"]["type"], $allowed)) {
+                    $destination = $folder . time() . $files["usercommentimage"]["name"];
+                    if (move_uploaded_file($files["usercommentimage"]['tmp_name'], $destination)) {
+                        $data["usercommentimage"] = $destination;
+                        $image = new Image();
+                        $image->resize($data["usercommentimage"], 700);
+                    }
+                } else {
+                    $this->errors["usercommentimage"] = "The file type is not supported.";
+                }
+            }
+
+            if (empty($this->errors)) {
+                $this->insert($data);
+                $ses = new Session();
+
+                $ses->set('comment_success', 'Your reply comment has been posted successfully.');
+                redirect('Blogview/'.$postid);
+            }
+        }
+    }
+
+}
