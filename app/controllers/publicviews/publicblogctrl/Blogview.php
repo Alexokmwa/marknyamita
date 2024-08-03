a486fc1390959ecc815ad081c4a672c5f1bf82e5
alexokemwa <okemwanalex@gmail.com>
2024-08-03 21:36:01 +0300

    handle blogview comeents and reply processing

--- app/controllers/publicviews/publicblogctrl/Blogview.php
+++ app/controllers/publicviews/publicblogctrl/Blogview.php
@@ -12,6 +12,9 @@ use app\models\Session;
 use app\models\Request;
 use app\models\Blogcomments;
 use app\models\Blogcommentsnotloggedin;
+use app\models\Blogloggedinreplies;
+use app\models\Blogloggedout;
+use app\models\User;
 
 class Blogview extends Controller
 {
@@ -20,8 +23,18 @@ class Blogview extends Controller
         $data['title'] = "View blog";
         $ses = new Session();
         $req = new Request();
+        // Instantiate Blogcomments model
+        $blogCommentsModelreplies = new Blogloggedinreplies();
+        $data['commentreply'] = $blogCommentsModelreplies->findAllcommentreplies();
+        $usercomentnotloggedinreplies = new Blogloggedout();
+        $data['commentreplynotlogged'] = $usercomentnotloggedinreplies->findAllcommentreplies();
+        $blogCommentsModel = new Blogcomments();
+        $usercomentnotloggedin = new Blogcommentsnotloggedin();
+        $data['comment'] = $blogCommentsModel->wherecomment(['postID' => $id]);
+        $data['commentnotlogged'] = $usercomentnotloggedin->wherecomment(['postID' => $id]);
 
         $userpost = new Adminpostsmodel();
+
         $data['rowpost'] = $userpost->first(['postID' => $id]);
 
         // Pager setup
@@ -31,33 +44,58 @@ class Blogview extends Controller
         $userpost->limit = $limit;
         $userpost->offset = $offset;
 
+        $adminpostdetail = new User();
+        $data['rowcreatorusers'] = $adminpostdetail->findAllusers();
         $adminpostdetail = new Adminaccounts();
         $data['rowcreator'] = $adminpostdetail->findAlladmin();
 
-        // Instantiate Blogcomments model
-        $blogCommentsModel = new Blogcomments();
-        $usercomentnotloggedin = new Blogcommentsnotloggedin();
-
+        
         if ($req->posted()) {
             $files = $req->files();
             $postid = $id;
+            
             $postData = $req->POST();
-
+            $commentid =$req->POST("commentID");
+            show($postData);
+            show($commentid);
+            $formType = $postData['form_type']; // Get the form type
+        
             if ($ses->isLoggedIn()) {
                 $userID = $ses->user("user_id");
-                $blogCommentsModel->addblogcomment($postData, $files, $userID, $postid);
+                switch ($formType) {
+                    case 'add_blog_comment':
+                        $blogCommentsModel->addblogcomment($postData, $files, $userID, $postid);
+                        break;
+                    case 'add_blog_comment_reply':
+                        $blogCommentsModelreplies->addblogcommentreply($postData, $commentid, $files, $userID, $postid);
+                        break;
+                    case 'add_blog_comment_reply_comment_from_notlogged':
+                        $usercomentnotloggedinreplies->addblogcommentnotloggedinreply($data,$commentid, $files, $userID, $postid);
+                        break;
+                }
             } else {
                 $userID = $ses->getSessionID();
-                $usercomentnotloggedin->addblogcommentnotloggedin($postData, $files, $userID, $postid);
+                switch ($formType) {
+                    case 'add_blog_comment_not_logged_in':
+                        $usercomentnotloggedin->addblogcommentnotloggedin($postData, $files, $userID, $postid);
+                        break;
+                    case 'add_blog_comment_reply_not_logged_in':
+                        $usercomentnotloggedinreplies->addblogcommentnotloggedinreply($data,$commentid, $files, $userID, $postid);
+                        break;
+                    case 'add_blog_comment_reply_not_logged_in_fromloggeduser':
+                        $blogCommentsModelreplies->addblogcommentreply($postData, $commentid, $files, $userID, $postid);
+                        break;
+                    
+                        
+                }
             }
-            // Optionally redirect to the same page to prevent form resubmission on refresh
-            // redirect('Blogview/'.$id);
         }
+        
 
         $data["pager"] = $pager;
         $data["user"] = $blogCommentsModel;
         $data["usernotloggedin"] = $usercomentnotloggedin;
-
+        // show($data);
         $this->view('publicviews/publicblog/blogviewpage', $data);
     }
 }
