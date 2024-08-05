8e189ab960ae6faadf732bd8fb048f127753d744
alexokemwa <okemwanalex@gmail.com>
2024-08-05 17:02:52 +0300

    delete excess nav items

--- app/views/publicviews/publicsharable/publicnavfooters/mainnav.view.php
+++ app/views/publicviews/publicsharable/publicnavfooters/mainnav.view.php
@@ -109,55 +109,14 @@ Header START -->
 										</a>
 									</li>
 
-									<!-- Notif item -->
-									<li>
-										<a href="#" class="list-group-item-action border-0 border-bottom d-flex p-3">
-											<div class="me-3">
-												<div class="avatar avatar-sm">
-													<img class="avatar-img rounded-circle" src="assets/images/avatar/02.jpg" alt="avatar">
-												</div>
-											</div>
-											<div>
-												<h6 class="mb-1">Larry Lawson deleted account</h6>
-												<span class="small"> <i class="bi bi-clock"></i> 6 min ago</span>
-											</div>
-										</a>
-									</li>
+									
 
-									<!-- Notif item -->
-									<li>
-										<a href="#" class="list-group-item-action border-0 border-bottom d-flex p-3">
-											<div class="me-3">
-												<div class="avatar avatar-sm">
-													<img class="avatar-img rounded-circle" src="assets/images/avatar/05.jpg" alt="avatar">
-												</div>
-											</div>
-											<div>
-												<h6 class="mb-1">Byan commented on your post</h6>
-												<span class="small"> <i class="bi bi-clock"></i> 10 min ago</span>
-											</div>
-										</a>
-									</li>
-
-									<!-- Notif item -->
-									<li>
-										<a href="#" class="list-group-item-action border-0 border-bottom d-flex p-3">
-											<div class="me-3">
-												<div class="avatar avatar-sm">
-													<img class="avatar-img rounded-circle" src="assets/images/avatar/03.jpg" alt="avatar">
-												</div>
-											</div>
-											<div>
-												<h6 class="mb-1">Settings updated</h6>
-												<span class="small"> <i class="bi bi-clock"></i> Yesterday</span>
-											</div>
-										</a>
-									</li>
+									
 								</ul>
 							</div>
 							<!-- Button -->
 							<div class="card-footer bg-transparent border-0 py-3 text-center position-relative">
-								<a href="#" class="stretched-link">See all incoming activity</a>
+								<a href="<?=ROOT?>Notifications" class="stretched-link">See all incoming activity</a>
 							</div>
 						</div>
 					</div>
