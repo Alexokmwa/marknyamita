426555f9e5614b457d9ba094e27c4be67cceecde
alexokemwa <okemwanalex@gmail.com>
2024-08-08 17:37:50 +0300

    retrive admin data to help indentify and display admin name

--- app/controllers/adminviews/admingeneralpagesctrl/posts/Adminpostcategories.php
+++ app/controllers/adminviews/admingeneralpagesctrl/posts/Adminpostcategories.php
@@ -8,6 +8,8 @@ use app\core\Admincontroller;
 use app\models\Adminsession;
 use app\models\Admincategories;
 use app\models\Request;
+use app\models\Adminpostsmodel;
+use app\models\Adminaccounts;
 
 class Adminpostcategories extends Admincontroller
 {
@@ -18,14 +20,21 @@ class Adminpostcategories extends Admincontroller
         if (!$ses->isLoggedIn()) {
             redirectadmin('Adminsignin');
         }
+        $adminpostdetail = new Adminaccounts();
+
+        $data['categorycreator'] = $adminpostdetail->findAlladmin();
+
         $user = new Admincategories();
-		$data['row'] = $user->findAllcategories(); 
+        $data['row'] = $user->findAllcategories();
+        $data['getcategories'] = new Adminpostsmodel();
         $data['admintitle'] = "Admin Categories";
         $data['admin'] = new Admincategories();
 
         $req = new Request();
         if ($req->posted()) {
-            $result = $data['admin']->adminaddcategory($req->POST());
+            $admid = $ses->adminuser("adminID");
+            $status = "active";
+            $result = $data['admin']->adminaddcategory($req->POST(), $admid,$status);
             echo json_encode($result);
             exit;
         }
