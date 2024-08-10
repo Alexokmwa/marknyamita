e5e206c491ce855a424dd0cb442f7931a2cc6db3
alexokemwa <okemwanalex@gmail.com>
2024-08-11 00:05:13 +0300

    add libraries header content

--- app/views/publicviews/publicgeneralpages/resource.view.php
+++ app/views/publicviews/publicgeneralpages/resource.view.php
@@ -4,8 +4,39 @@
  */
 renderpageHeader();
 ?>
+<nav class="myclassallignsecondnav navbar navbar-expand-xl navbar-light navbar-sticky header-static pt-2">
+	<div class="container-fluid">
 
-<h1 class="myclassmargintop">public resource view page design of tabs and subpages goes here</h1>
+		<ul class="navbar-nav  navbar-nav-scroll mx-auto d-flex flex-row flex-nowrap">
+			<!-- Nav Search -->
+			<li class="nav-item me-3"><a class="nav-link" href="<?=ROOT?>Blog"></i>Videos</a></li>
+			<li class="nav-item  me-3"><a class="nav-link" href="<?=ROOT?>Blog#trending"></i>Podcast</a></li>
+			<li class="nav-item me-3"><a class="nav-link" href="<?=ROOT?>Blog#recent"></i>pdfs</a></li>
+
+			</a></li>
+		</ul>
+		<div class="nav-item dropdown nav-search dropdown-toggle-icon-none d-md-none">
+			<a class="nav-link pe-0 dropdown-toggle" role="button" href="#" id="navSearch" data-bs-toggle="dropdown"
+				aria-expanded="false">
+				<i class="bi bi-search fs-4"> </i>
+			</a>
+			<div class="dropdown-menu dropdown-menu-end shadow rounded p-2" aria-labelledby="navSearch">
+				<form class="input-group">
+					<input class="form-control border-success" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-success m-0" type="submit">Search</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</nav>
+
+</header>
+<!-- =======================
+Header END -->
+<!-- **************** MAIN CONTENT START **************** -->
+<main class="mysmbottomspace pt-4">
+
+</main>
 <?php
 /**
  * html footer function
