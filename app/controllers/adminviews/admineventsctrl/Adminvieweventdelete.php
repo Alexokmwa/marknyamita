655bf5d8bc2606b8594c448b6e6e2fbfe23d38df
alexokemwa <okemwanalex@gmail.com>
2024-08-15 17:11:36 +0300

    create admin view event and view delete eventcontrollers

---
+++ app/controllers/adminviews/admineventsctrl/Adminvieweventdelete.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\controllers;
+
+// deny acess to app files and folders access.
+defined('ROOTPATH') or exit('Access Denied!');
+
+use app\core\Admincontroller;
+use app\models\Adminsession;
+use app\models\eventmodels\Admincreateeventmodel;
+
+/**
+ * admin Adminpostlist management class
+ */
+class Adminvieweventdelete extends Admincontroller
+{
+    public function adminindex($id = null)
+    {
+        $ses = new Adminsession();
+        if(!$ses->isLoggedIn()) {
+            redirectadmin('Adminsignin');
+        }
+
+        $eventpost = new Admincreateeventmodel();
+        $data['eventpost'] = $eventpost->first(['eventID' => $id]);
+
+        $data['admintitle'] = "Admin eventlist";
+        // show($data);
+        $this ->adminview('adminviews/adminevents/admindeleteevent', $data);
+    }
+}
