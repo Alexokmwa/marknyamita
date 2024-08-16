e71cf95f6d5463a079c6b2323bcc89fa4ba83a88
alexokemwa <okemwanalex@gmail.com>
2024-08-16 18:16:49 +0300

    add comment notification function

--- app/controllers/adminviews/admingeneralpagesctrl/Adminnotificationconroller.php
+++ app/controllers/adminviews/admingeneralpagesctrl/Adminnotificationconroller.php
@@ -8,8 +8,10 @@ defined('ROOTPATH') or exit('Access Denied!');
 use app\core\Admincontroller;
 use app\models\Adminsession;
 use app\models\Request;
-
+use app\models\Blogcomments;
+use app\models\Commentnotfcationmodel;
 use app\models\Notificationmodel;
+
 /**
  * admin not found management class
  */
@@ -18,20 +20,33 @@ class Adminnotificationconroller extends Admincontroller
     public function adminindex()
     {
         $data['admintitle'] = "Admin notification";
-        $notif = new Notificationmodel();
         $ses = new Adminsession();
+        $coomentsitemid = new Blogcomments();
+        $data['coomentsitemid'] = $coomentsitemid->findAllcomments();
         if(!$ses->isLoggedIn()) {
             redirectadmin('Adminsignin');
         }
+        $notif = new Notificationmodel();
+        $notifcomment = new Commentnotfcationmodel();
         $userID = $ses ->adminuser('adminID');
-    
-        $data['rows']=$notif->where(['ownerid'=>$userID]);
-        if($data['rows']){
-            foreach ($data['rows'] as $key =>$row){
-                $check_user = $notif->getRow("SELECT * FROM users WHERE user_id = :id limit 1",['id'=>$row->userID]);
-                $check_postphoto = $notif->getRow("SELECT * FROM blogposts WHERE postID = :id limit 1",['id'=>$row->Itemid]);
+
+        $data['rows'] = $notif->where(['ownerid' => $userID]);
+        $data['rowss'] = $notifcomment->where(['ownerid' => $userID]);
+        if($data['rowss']) {
+            foreach ($data['rowss'] as $key => $row) {
+                $check_user = $notifcomment->getRow("SELECT * FROM users WHERE user_id = :id limit 1", ['id' => $row->userID]);
+                $check_postphoto = $notifcomment->getRow("SELECT * FROM  blogposts WHERE postID = :id limit 1", ['id' => $row->Itemid]);
+                $data['rowss'][$key]->user_row =  $check_user;
+                $data['rowss'][$key]->item_row = $check_postphoto ;
+            }
+
+        }
+        if($data['rows']) {
+            foreach ($data['rows'] as $key => $row) {
+                $check_user = $notif->getRow("SELECT * FROM users WHERE user_id = :id limit 1", ['id' => $row->userID]);
+                $check_postphoto = $notif->getRow("SELECT * FROM blogposts WHERE postID = :id limit 1", ['id' => $row->Itemid]);
                 $data['rows'][$key]->user_row =  $check_user;
-                $data['rows'][$key]->item_row=$check_postphoto ;
+                $data['rows'][$key]->item_row = $check_postphoto ;
             }
 
         }
@@ -39,7 +54,7 @@ class Adminnotificationconroller extends Admincontroller
         $req = new Request();
         if ($req->posted()) {
             $postData = $req->POST();
-            $notif_id= $req->POST("id");
+            $notif_id = $req->POST("id");
 
             $formType = $postData['form_type']; // Get the form type
 
@@ -48,16 +63,25 @@ class Adminnotificationconroller extends Admincontroller
                 case 'delete_notification':
                     # code...
                     $notif->delete($notif_id);
+                    $ses = new Adminsession();
+
+                    $ses->set('comment_success', 'like notification deleted successfully');
+                    redirectadmin("Adminnotificationconroller");
+                    break;
+                case 'commentdelete_notification':
+                    # code...
+                    $notifcomment->delete($notif_id);
+                    $ses = new Adminsession();
+
+                    $ses->set('comment_success', 'comment notification deleted successfully');
+                    redirectadmin("Adminnotificationconroller");
                     break;
             }
-            $ses = new Adminsession();
-
-            $ses->set('comment_success', 'notification deleted successfully');
-       redirectadmin("Adminnotificationconroller");
 
         }
-        
-        $this->adminView('adminviews/adminnotification',$data);
+
+
+        $this->adminView('adminviews/adminnotification', $data);
 
         // echo  "404 error page not found";
     }
