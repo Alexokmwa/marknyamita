21e5cfe81dc96a0467ccf6651459a31c133a02ad
alexokemwa <okemwanalex@gmail.com>
2024-08-16 18:19:42 +0300

    add blog and event notification fetch handling

--- app/controllers/publicviews/Notifications.php
+++ app/controllers/publicviews/Notifications.php
@@ -6,30 +6,43 @@ namespace app\controllers;
 defined('ROOTPATH') or exit('Access Denied!');
 use app\core\Controller;
 use app\models\Adminsession;
+use app\models\Blognotificationmodel;
+use app\models\Eventnotificationmodel;
 use app\models\Notificationmodel;
 use app\models\Request;
+use app\models\Session;
 
 class Notifications extends Controller
 {
     public function index()
     {
-        $notif = new Notificationmodel();
+        $notifevent = new Eventnotificationmodel;
+        $notif = new Blognotificationmodel;
         $ses = new Adminsession();
-        // if(!$ses->isLoggedIn()) {
-        //     redirectadmin('Adminsignin');
-        // }
+        
         $userID = $ses ->adminuser('adminID');
 
         $data['rowspublic'] = $notif->where(['ownerid' => 0]);
+        $data['rowspublicevent'] = $notifevent->where(['ownerid' => 0]);
+        if($data['rowspublicevent']) {
+            foreach ($data['rowspublicevent'] as $key => $row) {
+                $check_user = $notifevent->getRow("SELECT * FROM admins WHERE adminID = :id limit 1", ['id' => $row->userID]);
+                $check_postphotoevent = $notifevent->getRow("SELECT * FROM events WHERE adminID = :id limit 1", ['id' => $row->userID]);
+                $check_postphotoevent = $notifevent->getRow("SELECT * FROM events WHERE eventID = :id limit 1", ['id' => $row->Itemid]);
+                $data['rowspublicevent'][$key]->user_row =  $check_user;
+                $data['rowspublicevent'][$key]->item_rowevent = $check_postphotoevent ;
+            }
+
+        }
         if($data['rowspublic']) {
             foreach ($data['rowspublic'] as $key => $row) {
                 $check_user = $notif->getRow("SELECT * FROM admins WHERE adminID = :id limit 1", ['id' => $row->userID]);
                 $check_postphoto = $notif->getRow("SELECT * FROM blogposts WHERE adminID = :id limit 1", ['id' => $row->userID]);
                 $check_postphoto = $notif->getRow("SELECT * FROM blogposts WHERE postID = :id limit 1", ['id' => $row->Itemid]);
-                $check_postphotoevent = $notif->getRow("SELECT * FROM events WHERE eventID = :id limit 1", ['id' => $row->Itemid]);
+                
                 $data['rowspublic'][$key]->user_row =  $check_user;
                 $data['rowspublic'][$key]->item_rowblog = $check_postphoto ;
-                $data['rowspublic'][$key]->item_rowevent = $check_postphotoevent ;
+                
             }
 
         }
@@ -41,17 +54,25 @@ class Notifications extends Controller
 
             $formType = $postData['form_type']; // Get the form type
 
-            $notif = new Notificationmodel();
+            
             switch ($formType) {
                 case 'delete_notification':
                     # code...
                     $notif->delete($notif_id);
+                    $ses = new Session;
+        
+                    $ses->set('comment_success', 'post notification deleted successfully');
+               redirect("Notifications");
+                    break;
+                case 'eventdelete_notification':
+                    # code...
+                    $notifevent->delete($notif_id);
+                    $ses = new Session;
+        
+                    $ses->set('comment_success', 'event notification deleted successfully');
+               redirect("Notifications");
                     break;
             }
-            $ses = new Adminsession();
-
-            $ses->set('comment_success', 'notification deleted successfully');
-       redirect("Notifications");
 
         }
         // show();
